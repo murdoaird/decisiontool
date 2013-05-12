56b0f118fd01e9c02ea8a3db3a5aea8c2211f520
murdo <murdo.aird@gmail.com>
2013-05-12 21:35:30 +0100

    working baseline app skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 ResumeBuilder::Application.routes.draw do
+  authenticated :user do
+    root :to => 'home#index'
+  end
+  root :to => "home#index"
+  devise_for :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
