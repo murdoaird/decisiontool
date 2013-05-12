44f2796dd665a6f79504a2c80c87796729fc693d
murdo <murdo.aird@gmail.com>
2013-05-12 07:50:00 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ResumeBuilder::Application.config.secret_token = '41da483a551bd13318eadd3454d1b7dc12cf4511467ac41e070a857bc3131ed530e61a82c7e8d35288e786cc709bbe201aea31bb3236d68fbcb9e93398925d42'
