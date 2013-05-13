8aa7afa6b1a73adde7b1dca1f4558cc0550a006d
murdo <murdo.aird@gmail.com>
2013-05-13 21:18:17 +0100

    Fixed issue#1  - passwords were too short so seed command was failing but not throwing error until added seed data via create! command

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 puts 'ROLES'
 YAML.load(ENV['ROLES']).each do |role|
-  Role.find_or_create_by_name({ :name => role }, :without_protection => true)
+  Role.create!({ :name => role }, :without_protection => true)
   puts 'role: ' << role
 end
 puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+user = User.create! :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
 puts 'user: ' << user.name
 user.add_role :admin
-user2 = User.find_or_create_by_email :name => 'User02', :email => 'blah@blah.com', :password => 'user02', :password_confirmation => 'user02'
+user2 = User.create! :name => 'User02', :email => 'blah@blah.com', :password => '1234567890', :password_confirmation => '1234567890'
 puts 'user: ' << user2.name
 user2.add_role :VIP
